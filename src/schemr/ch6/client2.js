852b6d28862573f52d559dea2f1d7512d0692d36
schemr <schemryj@gmail.com>
2020-08-27 21:44:11 +0900

    Refector extract function

--- src/schemr/ch6/client2.js
+++ src/schemr/ch6/client2.js
@@ -1,9 +1,12 @@
-import { acquireMember } from "./common";
+import { acquireMember, Member } from "./common";
 
 // 클라이언트 2
 // 연차 수당 계산 {(세전 월 급여 / 209) X 하루평균 근무시간} X 잔여 연차일수
-const aMember = acquireMember();
-export const basicWage = Math.floor(
-  (aMember.annual_salary / 12 / 209) * aMember.workingTime
-);
-export const bonus = basicWage * aMember.vacation;
+const rawMember = acquireMember();
+const aMember = new Member(rawMember);
+export const basicWage = aMember.basicWage;
+export const bonus = XXbonus(aMember);
+
+function XXbonus(aMember) {
+  return aMember.basicWage * aMember.vacation;
+}
