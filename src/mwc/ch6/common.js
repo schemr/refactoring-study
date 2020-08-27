e60cd92c73a3c6de40df4922969e478b6e2785d7
myeongwon <lighting1.front@gmail.com>
2020-08-27 21:45:00 +0900

    Add a Worker class

--- src/mwc/ch6/common.js
+++ src/mwc/ch6/common.js
@@ -8,3 +8,38 @@ const member = {
 export function acquireMember() {
   return member;
 }
+
+const MONTH_WORKING_TIME = 209;
+const MONTH_NUMBER = 12;
+export class Worker {
+  consturctor(data){
+    this.name = data.named;
+    this.annual_salary = data.annual_salary;
+    this.workingTime = data.workingTime;
+    this.vacation = data.vacation;
+  }
+
+  get name(){
+    return this.name;
+  }
+
+  get annual_salary(){
+    return this.annual_salary;
+  }
+
+  get workingTime(){
+    return this.workingTime;
+  }
+
+  get vacation(){
+    return this.vacation;
+  }
+
+  get basicWage(){
+    return this.basicWage();
+  }
+
+  basicWage(){
+    return (annual_salary / MONTH_NUMBER / MONTH_WORKING_TIME) * this.workingTime;
+  }
+}
