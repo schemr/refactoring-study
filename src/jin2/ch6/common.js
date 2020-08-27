8195c2aea19fe04074f69de28882d61d26ea6f71
이수진 <jmakeaz@gmail.com>
2020-08-27 21:16:27 +0900

    Add class

--- src/jin2/ch6/common.js
+++ src/jin2/ch6/common.js
@@ -1,10 +1,19 @@
 const member = {
   name: "홍길동",
-  annual_salary: 30000000,
+  annualSalary: 30000000,
   workingTime: 8,
   vacation: 15
 };
 
 export function acquireMember() {
   return member;
+}
+
+class Member {
+  constructor(data) {
+    this._name = data.name;
+    this._annualSalary = data.annualSalary;
+    this._workingTime = data.workingTime;
+    this._vacation = data.vacation;
+  }
 }
